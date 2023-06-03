a1208d9604c2195fee8322a7a5ed94f81f4b407f
Dev.Rahat Hasan <devrahat.hasan@gmail.com>
2023-06-04 01:27:54 +0600

    raddom color generate function

---
+++ main.js
@@ -0,0 +1,45 @@
+/* function calculate (){
+    const getInputNumberField1 = document.getElementById('input-field1')
+    const inputValuerectangle = getInputNumberField1.value;
+
+
+    const getInputNumberField2 = document.getElementById('input-field2')
+    const inputValueparallelogram = getInputNumberField2.value;
+
+   const getMultiplyOfValue = inputValuerectangle*inputValueparallelogram;
+
+   const ResultOutputField =document.getElementById('outputResult');
+  ResultOutputField.innerText =getMultiplyOfValue;
+}
+
+
+document.getElementById('rectangle-button').addEventListener('click', function () {
+    calculate ()
+    // console.log(inputValue);
+}) */
+
+
+
+
+
+// random color function
+
+  function randomBackgroundColor() {
+    var red = Math.floor(Math.random() * 256);     // Generate random value for red (0-255)
+    var green = Math.floor(Math.random() * 256);   // Generate random value for green (0-255)
+    var blue = Math.floor(Math.random() * 256);    // Generate random value for blue (0-255)
+  
+    var color = "rgb(" + red + ", " + green + ", " + blue + ")";   // Create the RGB color string
+    return color;
+  }
+  
+  
+  // random color execute on card
+  const getAllCard = document.querySelectorAll(".cards");
+  for(const getCards of getAllCard)
+  getCards.addEventListener('mouseover',function(){
+      getCards.style.backgroundColor =randomBackgroundColor();
+  })
+
+  // create table data function
+
